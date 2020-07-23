191077e50b0a17522d3e3c8f711996ed200dc488
kai <839823628@qq.com>
2020-07-23 20:32:07 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::get('signup','UsersController@create')->name('signup');
+
+
+Route::resource('users', 'UsersController');
