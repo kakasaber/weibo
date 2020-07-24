6b115afb3077f4ad3a805a9660356d3bdb48ac3e
kai <839823628@qq.com>
2020-07-24 11:07:45 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,9 +20,16 @@ class UsersController extends Controller
     {
         $this->validate($request,[
             'name'  => 'required|unique:users|max:50',
-            'email' => 'required|unique:users|max',
+            'email' => 'required|email|unique:users|max:255',
             'password'=> 'required|confirmed|min:6'
         ]);
-        reutrn;
+        $user = User::create([
+          'name' => $request->name,
+          'email'=> $request->email,
+          'password'=> bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
+        return redirect()->route('users.show',[$user]);
+
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.default');
-@section('title',$user->name);
+@extends('layouts.default')
+@section('title',$user->name)
 
-@section('content');
-{{csrf_field()}}
+@section('content')
+
 <div class="row">
   <div class="offset-md-2 col-md-8">
     <div class="col-md-12">
